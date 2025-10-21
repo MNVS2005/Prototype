1bce99102f9fdf7d82431ba6ac2048667b0d3a3c
mate <mateovaldisalva@gmail.com>
2025-10-21 10:12:45 +0200

    Solucionamos error con la conexion a la BD, Creamos la parte fronted

--- src/main/java/org/example/backend/SpringApplicationUser.java
+++ src/main/java/org/example/backend/SpringApplicationUser.java
@@ -1,12 +1,13 @@
 package org.example.backend;
 
+import org.example.backend.controller.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 @SpringBootApplication
 public class SpringApplicationUser {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringApplicationUser.class,args);
     }
